ff2f42e6b37270a60e6a9b59f002395d5f34487f
MoNgc89 <mgalindo.cruz@gmail.com>
2019-04-27 14:33:38 -0600

    4o commit corrigiendo acceso a la api

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,15 @@ let contenido = document.getElementById('info-friki')
   fetch('https://swapi.co/api/people')
    .then(response => response.json())//transforma los datos a json
     .then(function(data)  {
+      contenido.innerHTML = ``
       let personaje = data.results
       return personaje.map(function (persona){
+        //no acceder a los parametros de la lista desde la lista si no desde el elemento de la lista
         contenido.innerHTML += `<tr>
-          <td>${personaje.name}</td>
-         <td>${personaje.gender}</td>
-          <td>${personaje.height}</td>
-          <td>${personaje.mass}</td>
+          <td>${persona.name}</td>
+         <td>${persona.gender}</td>
+          <td>${persona.height}</td>
+          <td>${persona.mass}</td>
         </tr>`
       })
 
